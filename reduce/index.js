088783feb1cdb2045e2bd3e4576c9e05ba4ea0da
sqoobe <sqoobe96@gmail.com>
2025-02-24 11:48:44 +0100

    bugg med import

--- reduce/index.js
+++ reduce/index.js
@@ -1,7 +1,24 @@
-import { studentsArr } from '/studentsArr'
+///import buggg har bare array her....
+const studentsArr = [
+  {
+    name: "Mike",
+    grade: 75,
+  },
+  {
+    name: "Emma",
+    grade: 83,
+  },
+  {
+    name: "Seth",
+    grade: 66,
+  },
+];
 
 function calculateClassAverage(studentsArr) {
-
+  const totalGrades = studentsArr.reduce(function (total, currentStudent) {
+    return total + currentStudent.grade;
+  }, 0); //added 0 as second parameter in the function - makes the first value in the studentsArr to 0
+  return totalGrades / studentsArr.length;
 }
 
-// console.log(calculateClassAverage(studentsArr))
+console.log(calculateClassAverage(studentsArr));
