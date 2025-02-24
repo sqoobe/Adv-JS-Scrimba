eb162a538c2b125f745e8e9e4d28e543dcded264
sqoobe <sqoobe96@gmail.com>
2025-02-24 14:53:04 +0100

    start

--- contact search/index.js
+++ contact search/index.js
@@ -1,3 +1,6 @@
+import { contactsArr } from "./contactsData.js";
+
+console.log(contactsArr);
 /*
 Challenge:
 1. Wire up this search pattern app so that inputting 
@@ -5,14 +8,23 @@ Challenge:
    contact or contacts.
 */
 
-const patternSearchInput = document.getElementById('pattern-search-input')
-const patternSearchSubmit = document.getElementById('pattern-search-submit')
-const contactDisplay = document.getElementById('contact-display')
+const patternSearchInput = document.getElementById("pattern-search-input");
+const patternSearchSubmit = document.getElementById("pattern-search-submit");
+const contactDisplay = document.getElementById("contact-display");
+
+//event listener på search button
+patternSearchSubmit.addEventListener("click", function () {
+  findMatchingContacts(contactsArr, patternSearchInput.value);
+});
+
+function findMatchingContacts(contactsArr, pattern) {
+  console.log(contactsArr, pattern);
+}
 
 function renderContact(contactObj) {
-    const contactCard = document.createElement('aside')
-    contactCard.classList.add('contact-card')
-/*
+  const contactCard = document.createElement("aside");
+  contactCard.classList.add("contact-card");
+  /*
     The CSS for contact-card has been done for you. 
     The name, email and phone details can be placed in
     'p' elements and placed inside contact-card. 
