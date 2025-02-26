2f36a9e3e8f3b51c15e2aa373ea470554bc12c66
sqoobe <sqoobe96@gmail.com>
2025-02-26 10:24:40 +0100

    Super Challenge: Contact search - solution

--- contact search/index.js
+++ contact search/index.js
@@ -1,6 +1,5 @@
 import { contactsArr } from "./contactsData.js";
 
-console.log(contactsArr);
 /*
 Challenge:
 1. Wire up this search pattern app so that inputting 
@@ -18,15 +17,29 @@ patternSearchSubmit.addEventListener("click", function () {
 });
 
 function findMatchingContacts(contactsArr, pattern) {
-  console.log(contactsArr, pattern);
+  contactDisplay.innerHTML = "";
+  const regex = new RegExp(pattern, "i");
+  contactsArr
+    .filter(function (contact) {
+      return regex.test(contact.name);
+    })
+    .forEach(function (contact) {
+      renderContact(contact);
+    });
 }
 
 function renderContact(contactObj) {
+  const { name, email, phone } = contactObj;
   const contactCard = document.createElement("aside");
   contactCard.classList.add("contact-card");
-  /*
-    The CSS for contact-card has been done for you. 
-    The name, email and phone details can be placed in
-    'p' elements and placed inside contact-card. 
-*/
+  const nameElem = document.createElement("p");
+  const emailElem = document.createElement("p");
+  const phoneElem = document.createElement("p");
+  nameElem.innerText = name;
+  emailElem.innerText = email;
+  phoneElem.innerText = phone;
+  contactCard.appendChild(nameElem);
+  contactCard.appendChild(emailElem);
+  contactCard.appendChild(phoneElem);
+  contactDisplay.appendChild(contactCard);
 }
