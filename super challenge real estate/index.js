0f31e9c58a499bd4e1b87e1d89db5e42cfe4495e
sqoobe <sqoobe96@gmail.com>
2025-03-19 13:30:01 +0100

    super challenge real estate finito

--- super challenge real estate/index.js
+++ super challenge real estate/index.js
@@ -1,4 +1,29 @@
-function getPropertyHtml() {
+import propertyForSaleArr from "./properties/propertyForSaleArr.js";
+import placeholderPropertyObj from "./properties/placeholderPropertyObj.js";
+
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
+  return propertyArr
+    .map((property) => {
+      const { propertyLocation, priceGBP, roomsM2, comment, image } = property;
+      const roomSizeM2 = roomsM2.reduce((total, current) => total + current);
+      return `
+    <section class="card">
+        <img src="./images/${image}">
+        <div class="card-right">
+            <h2>${propertyLocation}</h2>
+            <h3>£ ${priceGBP}</h3>
+            <p>${comment}</p>
+            <h3>${roomSizeM2} m&sup2;</h3>
+        </div>
+    </section>`;
+    })
+    .join(""); //fjerner komma etter hver card.;
+}
+
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+document.getElementById("container").innerHTML =
+  getPropertyHtml(propertyForSaleArr);
+
 /*
 SUPER CHALLENGE 💪
 
@@ -27,7 +52,3 @@ This is the HTML template 👇. Replace everything in UPPERCASE with property da
     </div>
 </section> 
 */
-}
-
-/***** Modify 👇 by adding an argument to the function call ONLY. *****/
-document.getElementById('container').innerHTML = getPropertyHtml()
